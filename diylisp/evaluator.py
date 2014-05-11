e7c44f8e2a0b15dd78daf71efdc34cfb1d7bd76f
brandly <brandly13@gmail.com>
2014-05-10 20:38:11 -0400

    defining and calling some functions (not all tho)

--- diylisp/evaluator.py
+++ diylisp/evaluator.py
@@ -53,14 +53,15 @@ theMaths = {
 }
 
 def evaluate(ast, env):
+    print ast
 
-    def evalIfList(thing):
-        if isinstance(thing, list):
-            thing = evaluate(thing, env)
+    def evalIfNeeded(thing, e=env):
+        if isinstance(thing, (list, str)):
+            thing = evaluate(thing, e)
         return thing
 
     def ensureInt(thing):
-        thing = evalIfList(thing)
+        thing = evalIfNeeded(thing)
         if isinstance(thing, int):
             return thing
         else:
@@ -74,12 +75,12 @@ def evaluate(ast, env):
         result = ast[1]
 
     elif ast[0] == 'atom':
-        ast[1] = evalIfList(ast[1])
+        ast[1] = evalIfNeeded(ast[1])
         result = not isinstance(ast[1], list)
 
     elif ast[0] == 'eq':
-        ast[1] = evalIfList(ast[1])
-        ast[2] = evalIfList(ast[2])
+        ast[1] = evalIfNeeded(ast[1])
+        ast[2] = evalIfNeeded(ast[2])
 
         # Lists are never equal, because lists are not atoms
         if isinstance(ast[1], list) or isinstance(ast[2], list):
@@ -92,12 +93,12 @@ def evaluate(ast, env):
         result = math(ensureInt(ast[1]), ensureInt(ast[2]))
 
     elif ast[0] == 'if':
-        ast[1] = evalIfList(ast[1])
+        ast[1] = evalIfNeeded(ast[1])
 
         if ast[1]:
-            result = evalIfList(ast[2])
+            result = evalIfNeeded(ast[2])
         else:
-            result = evalIfList(ast[3])
+            result = evalIfNeeded(ast[3])
 
     elif ast[0] == 'define':
         if len(ast) != 3:
@@ -106,9 +107,33 @@ def evaluate(ast, env):
         if not isinstance(ast[1], str):
             raise LispError("non-symbol")
 
-        env.set(ast[1], evalIfList(ast[2]))
+        env.set(ast[1], evalIfNeeded(ast[2]))
         result = None
 
+    elif ast[0] == 'lambda':
+        if len(ast) != 3:
+            raise LispError("Wrong number of arguments")
+
+        if not isinstance(ast[1], list):
+            raise LispError("Params must be a list")
+
+        result = Closure(env, ast[1], ast[2])
+
+    elif isinstance(ast[0], Closure):
+        params = ast[0].params
+        env = ast[0].env
+        args = ast[1:]
+
+        argsMap = {}
+        for i, param in enumerate(params):
+            argsMap[param] = evalIfNeeded(args[i], env)
+
+        result = evaluate(ast[0].body, env.extend(argsMap))
+
+    elif isinstance(ast, list):
+        # ast[0] is assumed to be a function name
+        ast[0] = env.lookup(ast[0])
+        result = evaluate(ast, ast[0].env)
     else:
         result = env.lookup(ast)
 
